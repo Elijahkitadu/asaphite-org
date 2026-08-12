be3334c135222d0214ee95b02b45c1f45db29ac7
Elijahkitadu <elijahkitadu20@gmail.com>
2026-08-12 06:59:21 +0000

    Location update

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,7 +7,7 @@ export const SITE_URL         = process.env.NEXT_PUBLIC_SITE_URL || 'https://the
 export const SITE_EMAIL       = 'info@theasaphitesfoundation.org'
 export const SITE_PHONE       = '+255 767 069 313'
 export const SITE_WHATSAPP    = '+255767069313'
-export const SITE_LOCATION    = 'Arusha, Tanzania'
+export const SITE_LOCATION    = 'Kwa mwanajeshi street, nearby Mashariki street, Select Ward ward, Arumeru District, Arusha Region, P. O BOX 3013'
 
 export const NAV_ITEMS: NavItem[] = [
   { label: 'Home',            href: '/'          },
